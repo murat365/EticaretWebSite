729a179c3ea9fb48097fa754fa3ee6c8f29e5c27
murat365 <muratsagdic97@gmail.com>
2024-03-21 13:32:25 +0300

    Sipariş Yönetimi geliştirildi.

--- Abc.MvcWebUI/Controllers/CartController.cs
+++ Abc.MvcWebUI/Controllers/CartController.cs
@@ -28,7 +28,7 @@ namespace Abc.MvcWebUI.Controllers
             return RedirectToAction("Index");
         }
 
-        public ActionResult RemoveFromCart(int Id)
+        public ActionResult RemoveCart(int Id)
         {
             var product = db.Products.Where(i => i.Id == Id).FirstOrDefault();
 
@@ -50,5 +50,69 @@ namespace Abc.MvcWebUI.Controllers
             return cart;
         }    
 
+        public PartialViewResult Summary()
+        {
+            return PartialView(GetCart());
+        }
+
+        public ActionResult Checkout() 
+        {
+            return View(new ShippingDetails());
+        }
+
+        [HttpPost]
+        public ActionResult Checkout(ShippingDetails entity)
+        {
+            var cart = GetCart();
+
+            if (cart.CartLines.Count==0)
+            {
+                ModelState.AddModelError("UrunYokError", "Sepetinizde ürün bulunmamaktadır");
+            }
+
+            if (ModelState.IsValid)
+            {
+                SaveOrder(cart, entity);
+                cart.Clear();
+                return View("Completed");
+            }
+            else
+            {
+                return View(entity);
+            }
+        }
+
+        private void SaveOrder(Cart cart, ShippingDetails entity)
+        {
+            var order = new Order();
+
+            order.OrderNumber= "A" + (new Random()).Next(11111, 99999).ToString();
+            order.Total = cart.Total();
+            order.OrderDate = DateTime.Now;
+            
+            order.Username=entity.Username;
+            order.AdresBasligi=entity.AdresBasligi;
+            order.Adres=entity.Adres;
+            order.Sehir=entity.Sehir;
+            order.Semt=entity.Semt;
+            order.Mahalle=entity.Mahalle;
+            order.PostaKodu=entity.PostaKodu;
+
+            order.OrderLines = new List<OrderLine>();
+
+            foreach (var pr in cart.CartLines)
+            {
+                var orderline = new OrderLine();
+                orderline.Quantity = pr.Quantity;
+                orderline.Price = pr.Quantity*pr.Product.Price;
+                orderline.ProductId = pr.Product.Id;
+
+                order.OrderLines.Add(orderline);
+            }
+
+            db.Orders.Add(order);
+            db.SaveChanges();
+
+        }
     }
 }

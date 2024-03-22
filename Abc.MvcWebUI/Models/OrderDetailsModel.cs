b64369cabcaacb653a380c1632b423f9750e1fef
murat365 <muratsagdic97@gmail.com>
2024-03-22 15:08:39 +0300

    Yönetici paneli geliştirildi.

--- Abc.MvcWebUI/Models/OrderDetailsModel.cs
+++ Abc.MvcWebUI/Models/OrderDetailsModel.cs
@@ -9,6 +9,7 @@ namespace Abc.MvcWebUI.Models
     public class OrderDetailsModel
     {
         public int OrderId { get; set; }
+        public string UserName { get; set; }
         public string OrderNumber { get; set; }
         public double Total { get; set; }
         public DateTime OrderDate { get; set; }

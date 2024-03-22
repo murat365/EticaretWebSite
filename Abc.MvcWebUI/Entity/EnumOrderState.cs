8c601decb0b2482e43a268cf98590c054232a860
murat365 <muratsagdic97@gmail.com>
2024-03-22 11:21:00 +0300

    Kullanıcı sipariş bilgi liste ve detay sayfaları eklendi.

---
+++ Abc.MvcWebUI/Entity/EnumOrderState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Abc.MvcWebUI.Entity
+{
+    public enum EnumOrderState
+    {
+        Waiting,
+        Completed
+    }
+}
